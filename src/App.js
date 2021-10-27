6f7aabff9af6ef17242f8b1c22f90d7fe0a6884b
cloud <the_1975@naver.com>
2021-10-27 22:34:11 +0900

    211027

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Kiosk from "./components/Ex1";
+import CountContainer from "./components/Counter/CountContainer";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Kiosk></Kiosk>
     </div>
   );
 }
